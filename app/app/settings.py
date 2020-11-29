2223544a9b8d73b5bcadb2af310e91d6981c9920
asilbek5999 <asilbek.aleiv.1999@mail.ru>
2020-11-29 17:46:28 +0500

    feature: Added cutom user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+AUTH_USER_MODEL = 'core.user'
 STATIC_URL = '/static/'
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Testing user creation with email successfully"""
+        email = "asilbek.aliev.1999@gmail.com"
+        password = "Qwerty12345"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_user_normalized_email(self):
+        email = "asilbek@NOVALAB.UZ"
+        password = "12345"
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email.lower())
+
+    def test_user_invalid_email(self):
+        """Test creating user with no  email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, '12345')
+
+    def test_create_new_super_user(self):
+        user = get_user_model().objects.create_superuser(
+            'someemail@mail.ru',
+            'password12345',
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
